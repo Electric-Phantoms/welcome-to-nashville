850aa12c745067e724eac431a10268994b689e98
Sullivan Pierce <sullypierce@gmail.com>
2019-10-21 15:52:20 -0500

    added some comments

--- src/scripts/getRestaurantResult.js
+++ src/scripts/getRestaurantResult.js
@@ -4,8 +4,11 @@ const getRestaurantResult = (searchInput) => {
         //now we are taking that big object that returned from our fetch call and picking out just the name and address for the first 10 restaurants and putting them together as strings in an array
         let dataArray = [];
         for (i=0; i<10; i++) {
+            //putting the name of each restaurant in a string
            const nameString = parsedData.restaurants[i].restaurant.name;
+           //putting the address of each restaurant in a string
            const addressString = parsedData.restaurants[i].restaurant.location.address;
+           //pushing an element to the array with the name and address of each restaurant
            dataArray.push(`${nameString}: ${addressString}`);
         }
         console.log(dataArray);
