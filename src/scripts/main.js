9dfa73eaa5670a2f29dbac53ee2a3c89fe0f5d26
Chase Fite <chasef7031@gmail.com>
2019-10-22 15:25:09 -0500

    implemented search clearing functionality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 let saveSearchID = ""
 
+const clearSearch = (searchButtonID) => {
+    document.querySelector(`${searchButtonID}`).reset()
+}
+
+const clearResults = () => {
+    document.querySelector("#listContainer").innerHTML = ""
+}
+
 populateHTML()
 
 // puts the features on the dropdown menu of the DOM - reference parkDropdown.js
@@ -11,20 +19,28 @@ for (feature of parkFeatures) {
 //each event listener has a specific function to go and do a fetch call from its specific API and post the results on the dom with a save button for the info from each specific thing
 
 document.querySelector("#getParkResult").addEventListener("click", () => {
+    clearResults()
     saveSearchID = "savePark"
     const re = / /g
     getParkResult(document.querySelector("#parkSearch").value.replace(re, "_"))
 })
 document.querySelector("#getRestaurantResult").addEventListener("click", () => {
+    clearResults()
+    // document.querySelector("#listContainer").innerHTML = ""
     saveSearchID = "saveRestaurant"
     getRestaurantResult(document.querySelector("#restaurantSearch").value)
+    document.querySelector("#restaurantSearch").value = ""
 })
 document.querySelector("#getMeetupResult").addEventListener("click", () => {
+    clearResults()
     saveSearchID = "saveMeetup"
-    getMeetupResult(document.querySelector("#meetupSearch").value)    
+    getMeetupResult(document.querySelector("#meetupSearch").value)
+    document.querySelector("#meetupSearch").value = ""
 })
 document.querySelector("#getConcertResult").addEventListener("click", () => {
+    clearResults()
     saveSearchID = "saveConcert"
     getConcertResult(document.querySelector("#concertSearch").value)
+    document.querySelector("#concertSearch").value = ""
 })
 
