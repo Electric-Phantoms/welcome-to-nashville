6bee8c3c70e64325e73bd15d377adbe791eafa4c
Sullivan Pierce <sullypierce@gmail.com>
2019-10-22 15:32:25 -0500

    added no results found message to populate in the results div if the searched for term has no results

--- src/scripts/fetchFromZomato.js
+++ src/scripts/fetchFromZomato.js
@@ -12,8 +12,12 @@ const fetchFromZomato = (searchTerm) => {
             }
             
             //this fetch call then uses typeID to search restaurants in Nashville for that type of food and returns a big object with the results
+            if (typeID===9999) {
+                document.querySelector("#listContainer").innerText = "NO RESULTS FOUND";
+            }
+            else {
           return  fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&cuisines=${typeID}&start=first&sort=rating&apikey=${restKey}`)
-    .then(restaurantJSONInfo => restaurantJSONInfo.json())
+    .then(restaurantJSONInfo => restaurantJSONInfo.json())}
     
     
         }
