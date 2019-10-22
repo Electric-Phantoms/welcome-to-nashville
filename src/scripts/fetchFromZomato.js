4a9f7472673d7dbf11e60675d38264eba31c45e1
Sullivan Pierce <sullypierce@gmail.com>
2019-10-22 09:39:18 -0500

    fixed fetch call so it actually returns what it is supposed to based on searching for type of cuisine

--- src/scripts/fetchFromZomato.js
+++ src/scripts/fetchFromZomato.js
@@ -1,20 +1,23 @@
 const fetchFromZomato = (searchTerm) => {
     //this fetch call searchs and finds the right establishment ID for the type of food you searched and saves it to typeID
-    return fetch(`https://developers.zomato.com/api/v2.1/establishments?city_id=1138&apikey=${restKey}`)
+    return fetch(`https://developers.zomato.com/api/v2.1/cuisines?city_id=1138&apikey=${restKey}`)
     .then(jsonData => jsonData.json())
     .then(parsedData => {
         let typeID = 0;
-        for (i=0; i<parsedData.establishments.length; i++) {
-            if (parsedData.establishments[i].establishment.name.includes(searchTerm)) {
-                typeID = parsedData.establishments[i].establishment.id;
+        for (i=0; i<parsedData.cuisines.length; i++) {
+            if (parsedData.cuisines[i].cuisine.cuisine_name===searchTerm) {
+                typeID = parsedData.cuisines[i].cuisine.cuisine_id;
+                }
+                console.log(typeID)
             }
+            console.log(typeID)
             //this fetch call then uses typeID to search restaurants in Nashville for that type of food and returns a big object with the results
-          return  fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&establishment_type=${typeID}&start=first&sort=rating&apikey=${restKey}`)
+          return  fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&cuisines=${typeID}&start=first&sort=rating&apikey=${restKey}`)
     .then(restaurantJSONInfo => restaurantJSONInfo.json())
     
     
         }
-    })
+    )}
     
         
    
@@ -22,4 +25,3 @@ const fetchFromZomato = (searchTerm) => {
     
     
      
-}
--- src/scripts/getRestaurantResult.js
+++ src/scripts/getRestaurantResult.js
@@ -3,16 +3,19 @@ const getRestaurantResult = (searchInput) => {
     .then(parsedData => {
         //now we are taking that big object that returned from our fetch call and picking out just the name and address for the first 10 restaurants and putting them together as strings in an array
         let dataArray = [];
+        
         for (i=0; i<10; i++) {
+            
+            if (parsedData.restaurants[i]){
             //putting the name of each restaurant in a string
            const nameString = parsedData.restaurants[i].restaurant.name;
-           //putting the address of each restaurant in a string
+        //    //putting the address of each restaurant in a string
            const addressString = parsedData.restaurants[i].restaurant.location.address;
-           //pushing an element to the array with the name and address of each restaurant
+        //    //pushing an element to the array with the name and address of each restaurant
            dataArray.push(`${nameString}: ${addressString}`);
-        }
+        }}
         console.log(dataArray);
-        console.log(parsedData);
+        
         // putDataOnDom();
     })
 }
