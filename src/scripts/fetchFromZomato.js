2ee20942c450bd41cb7fe5c27659e41168beecf1
Sullivan Pierce <sullypierce@gmail.com>
2019-10-22 15:24:55 -0500

    search is no longer case sensitive

--- src/scripts/fetchFromZomato.js
+++ src/scripts/fetchFromZomato.js
@@ -3,14 +3,14 @@ const fetchFromZomato = (searchTerm) => {
     return fetch(`https://developers.zomato.com/api/v2.1/cuisines?city_id=1138&apikey=${restKey}`)
     .then(jsonData => jsonData.json())
     .then(parsedData => {
-        let typeID = 0;
+        let typeID = 9999;
         for (i=0; i<parsedData.cuisines.length; i++) {
-            if (parsedData.cuisines[i].cuisine.cuisine_name===searchTerm) {
+            if (parsedData.cuisines[i].cuisine.cuisine_name.toUpperCase()===searchTerm.toUpperCase()) {
                 typeID = parsedData.cuisines[i].cuisine.cuisine_id;
                 }
-                console.log(typeID)
+                
             }
-            console.log(typeID)
+            
             //this fetch call then uses typeID to search restaurants in Nashville for that type of food and returns a big object with the results
           return  fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=1138&entity_type=city&cuisines=${typeID}&start=first&sort=rating&apikey=${restKey}`)
     .then(restaurantJSONInfo => restaurantJSONInfo.json())
