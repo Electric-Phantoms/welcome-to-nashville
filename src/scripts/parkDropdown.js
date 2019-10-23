108ce07f5580f52a4cd9de29d3e9ffb899d3c27d
Sophia Hoffman <sophiahoffman@hotmail.com>
2019-10-23 02:55:29 -0500

    breakout parkFetch function from getParkResults function and HTML review

--- src/scripts/parkDropdown.js
+++ src/scripts/parkDropdown.js
@@ -1,8 +1,7 @@
 // establish park features that will be included in a drop down menu
 const parkFeatures = ["community_center", "nature_center", "playground", "ada_accessible", "restrooms_available", "picnic_shelters", "dog_park", "baseball_fields", "basketball_courts", "volleyball", "soccer_fields", "football_multi_purpose_fields", "tennis_courts", "disc_golf", "skate_park", "swimming_pool", "spray_park", "golf_course", "walk_jog_paths", "hiking_trails", "horse_trails", "mountain_bike_trails", "boat_launch", "camping_available_by_permit", "fishing_by_permit", "lake", "canoe_launch", "community_garden", "historic_features"]
-const parkFeaturesDisplay = []
-// const parkSearchEl = document.querySelector("#parkSearch")
 
+// formats dropdown menu items to remove underscore for appearance; added back in for search
 const newFeatureOption = (feature) => {
     const re = /_/g
     const newOption = document.createElement("option")
@@ -10,5 +9,12 @@ const newFeatureOption = (feature) => {
     newOption.innerHTML = feature.replace(re,' ')
     newOption.id = `feature-${feature}`
     document.querySelector("#parkSearch").appendChild(newOption)
+}
+
+// function to populate dropdown menu called on main.js
+const populateParkDropdown = parkfeatures => {
+    for (feature of parkFeatures) {
+    newFeatureOption(feature)
+    }
 }
 
