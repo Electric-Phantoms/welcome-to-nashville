685d121a3ae4c769125af9f5e53e2f27ef024fae
Chase Fite <chasef7031@gmail.com>
2019-10-24 11:05:17 -0500

    added top line break, center page styling, and background image

--- src/scripts/htmlBody.js
+++ src/scripts/htmlBody.js
@@ -6,59 +6,59 @@ const populateHTML = () => {
         <h1>Welcome to Nashville</h1>
     </div>
 
-    <div></div>
+    <div id="mainDiv">
+        <div id="searchContainer">
+            <h2>Search for stuff to do today</h2>
 
-    <div id="searchContainer">
-        <h2>Search for stuff to do today</h2>
+            <div class="textSearchButtonPair">
 
-        <div class="textSearchButtonPair">
+                <label class="labelStyling" id="searchLabelPark" for="parkSearch">Search for Parks</label>
 
-            <label class="labelStyling" id="searchLabelPark" for="parkSearch">Search for Parks</label>
+                <select name="parkFeatures" id="parkSearch"></select>
+                <button class="button searchButton" id="getParkResult">Search</button>            
+            </div>
 
-            <select name="parkFeatures" id="parkSearch"></select>
-            <button class="button searchButton" id="getParkResult">Search</button>            
+            <div class="textSearchButtonPair">
+
+                <label class="labelStyling" id="searchLabelRestaurant" for="restaurantSearch">Search for Restaurants</label>
+
+                <input type="text" id="restaurantSearch">
+                <button class="button searchButton" id="getRestaurantResult">Search</button>            
+            </div>
+
+            <div class="textSearchButtonPair">
+
+                <label class="labelStyling" id="searchLabelMeetup" for="meetupSearch">Search for Meetups</label>
+
+                <input type="text" id="meetupSearch">
+                <button class="button searchButton" id="getMeetupResult">Search</button>            
+            </div>
+
+            <div class="textSearchButtonPair">
+
+                <label class="labelStyling" id="searchLabelConcert" for="concertSearch">Search for Concerts</label>
+
+                <input type="text" id="concertSearch">
+                <button class="button searchButton" id="getConcertResult">Search</button>            
+            </div>
         </div>
 
-        <div class="textSearchButtonPair">
+        <div id="resultsContainer">
+            <h2>Results</h2>
 
-            <label class="labelStyling" id="searchLabelRestaurant" for="restaurantSearch">Search for Restaurants</label>
-
-            <input type="text" id="restaurantSearch">
-            <button class="button searchButton" id="getRestaurantResult">Search</button>            
+            <div id="listContainer">
+                
+            </div>
         </div>
 
-        <div class="textSearchButtonPair">
+        <div id="itineraryContainer">
+            <h2>My itinerary</h2>
 
-            <label class="labelStyling" id="searchLabelMeetup" for="meetupSearch">Search for Meetups</label>
-
-            <input type="text" id="meetupSearch">
-            <button class="button searchButton" id="getMeetupResult">Search</button>            
+            <div class="saveResult Park" id="savePark">Park: </div>
+            <div class="saveResult Restaurant" id="saveRestaurant">Restaurant: </div>
+            <div class="saveResult Meetup" id="saveMeetup">Meetup: </div>
+            <div class="saveResult Concert" id="saveConcert">Concert: </div>
         </div>
-
-        <div class="textSearchButtonPair">
-
-            <label class="labelStyling" id="searchLabelConcert" for="concertSearch">Search for Concerts</label>
-
-            <input type="text" id="concertSearch">
-            <button class="button searchButton" id="getConcertResult">Search</button>            
-        </div>
-    </div>
-
-    <div id="resultsContainer">
-        <h2>Results</h2>
-
-        <div id="listContainer">
-            
-        </div>
-    </div>
-
-    <div id="itineraryContainer">
-        <h2>My itinerary</h2>
-
-        <div class="saveResult Park" id="savePark">Park: </div>
-        <div class="saveResult Restaurant" id="saveRestaurant">Restaurant: </div>
-        <div class="saveResult Meetup" id="saveMeetup">Meetup: </div>
-        <div class="saveResult Concert" id="saveConcert">Concert: </div>
     </div>
     `
 }
