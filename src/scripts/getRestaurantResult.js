a35c1d3b65b7d4ae1fb04665b838b998c55c4e8e
Sullivan Pierce <sullypierce@gmail.com>
2019-10-24 13:42:08 -0500

    minor changes, fixed error that occurs when there are no search results

--- src/scripts/getRestaurantResult.js
+++ src/scripts/getRestaurantResult.js
@@ -3,21 +3,21 @@ const getRestaurantResult = (searchInput) => {
         .then(parsedData => {
             //now we are taking that big object that returned from our fetch call and picking out just the name and address for the first 10 restaurants and putting them together as strings in an array
             let dataArray = [];
+            if (parsedData) {
+                for (i = 0; i < 10; i++) {
 
-            for (i = 0; i < 10; i++) {
-
-                if (parsedData.restaurants[i]) {
-                    //putting the name of each restaurant in a string
-                    const nameString = parsedData.restaurants[i].restaurant.name;
-                    //    //putting the address of each restaurant in a string
-                    const addressString = parsedData.restaurants[i].restaurant.location.address;
-                    //    //pushing an element to the array with the name and address of each restaurant
-                    dataArray.push(`${nameString}: ${addressString}`);
+                    if (parsedData.restaurants[i]) {
+                        //putting the name of each restaurant in a string
+                        const nameString = parsedData.restaurants[i].restaurant.name;
+                        //    //putting the address of each restaurant in a string
+                        const addressString = parsedData.restaurants[i].restaurant.location.address;
+                        //    //pushing an element to the array with the name and address of each restaurant
+                        dataArray.push(`${nameString}: ${addressString}`);
+                    }
                 }
+                displayResults(dataArray)
+                saveButtonFunctionality(1, dataArray)
             }
-            displayResults(dataArray)
-            saveButtonFunctionality(1, dataArray)
-
             // putDataOnDom();
         })
 }
